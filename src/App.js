ddab81633c9d95764a50e1554a0c7f941b0814fa
ada lovelace <williambookman@pursuit.org>
2024-01-01 19:33:54 -0500

    Completed Nested Dynamic Routes exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog';
+import Nav from './components/Nav';
+
+
 
 function App() {
   return (
+    <BrowserRouter> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav />
+
+    <Blog /> 
+    {/* <h1>Home Page</h1>  */}
     </div>
+</BrowserRouter>
   );
 }
+
+// const Home = () => {
+//   return (
+// <div> 
+// <h1>Home</h1>
+// </div>
+//   )
+//  }
 
 export default App;
