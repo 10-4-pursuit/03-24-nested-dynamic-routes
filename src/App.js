a0d0393f73d6f69085217df23e4efbf8ec3a1882
MoniqueC10-4 <monique.charles13@gmail.com>
2024-02-17 17:28:31 -0500

    Completed Nested Dynamic Routes exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useParams, BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import blogData from '../src/data/blog.json'; 
 import './App.css';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <Link to='/blog'>Blog Posts</Link>
+        </nav>
+        <Routes>
+          <Route path='/blog' element={<PostList />} />
+          <Route path='/blog/:postId' element={<PostDetail />} />
+        </Routes>
+      </div>
+    </Router>  
+  );
+}
+
+
+function PostList() {
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      {Object.keys(blogData.posts).map((postId) => {
+      const post = blogData.posts[postId];
+      return (
+      <div key={postId}>
+        <Link to={`/blog/${postId}`}>{post.title}</Link>
+        <p>{post.summary}</p>
+      </div>
+      );
+    })}
     </div>
   );
 }
 
+function PostDetail() {
+  let { postId } = useParams();
+  const post = blogData.posts[postId];
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+      <p>Author: {post.author}</p>
+      <p>Date: {post.date}</p>
+      <p>Tags: {post.tags.join(', ')}</p>
+    </div>
+  );
+}
 export default App;
